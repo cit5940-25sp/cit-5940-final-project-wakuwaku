d564d12bdfafddd454d174aee4e54889e29d0b1e
Zhuy01 <zhuyf@seas.upenn.edu>
2025-05-08 16:54:45 -0400

    Win Condition based on Genre

---
+++ src/GenreWinCondition.java
@@ -0,0 +1,43 @@
+/**
+ * Win condition: first to name a certain number of movies of a specific genre.
+ */
+public class GenreWinCondition implements WinConditionStrategy {
+    private final String genre;
+    private final int targetCount;
+    private int currentCount = 0;
+
+    public GenreWinCondition(String genre, int targetCount) {
+        this.genre = genre.toLowerCase();
+        this.targetCount = targetCount;
+    }
+
+    @Override
+    public void recordMovie(Movie movie) {
+        if (movie.getGenres().stream()
+                .anyMatch(g -> g.equalsIgnoreCase(genre))) {
+            currentCount++;
+        }
+    }
+
+    @Override
+    public boolean isSatisfied() {
+        return currentCount >= targetCount;
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format("First to name %d %s movie(s)", targetCount, genre);
+    }
+
+    @Override
+    public int getCurrentCount() {
+        return currentCount;
+    }
+
+    @Override
+    public int getTargetCount() {
+        return targetCount;
+    }
+}
+
+
